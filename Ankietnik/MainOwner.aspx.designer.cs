3e19a3705e9b8c1e7e4cf3cf899a062174147737
doradelikat <dora.delikat@gmail.com>
2020-05-21 22:32:05 +0200

    MainOwner

---
+++ Ankietnik/MainOwner.aspx.designer.cs
@@ -0,0 +1,78 @@
+﻿//------------------------------------------------------------------------------
+// <generowany automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Modyfikacje tego pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowany automatycznie>
+//------------------------------------------------------------------------------
+
+namespace Ankietnik {
+    
+    
+    public partial class MainOwner {
+        
+        /// <summary>
+        /// Kontrolka Message.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Message;
+        
+        /// <summary>
+        /// Kontrolka labelMessage.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label labelMessage;
+        
+        /// <summary>
+        /// Kontrolka HeaderSprawdźBrak.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label HeaderSprawdźBrak;
+        
+        /// <summary>
+        /// Kontrolka HeaderSprawdź.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label HeaderSprawdź;
+        
+        /// <summary>
+        /// Kontrolka ListSprawdz.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ListSprawdz;
+        
+        /// <summary>
+        /// Kontrolka ButtonSprawdz.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonSprawdz;
+        
+        /// <summary>
+        /// Kontrolka reqPassword.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator reqPassword;
+    }
+}
